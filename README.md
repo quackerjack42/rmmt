b9330dd7efbc9c9455eefc83a207e555299e9439
Niklas Weiss <niklas.weiss@tum.de>
2017-12-01 14:02:47 +0100

    Add some how-tos and reset index.js

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# rmmt
+# Dev Doku
+
+Deployment:
+
+1. Lokal Committen
+2. Push auf Remote
+3. npm run-script deploy im lokalen Repository ausführen --> Pushed Remote Commit auf Server und started index.js
+
+SSH:
+
+ssh -i "~/.ssh/testKey.pem" ubuntu@ec2-18-195-150-108.eu-central-1.compute.amazonaws.com
+
+Übersicht der laufenden Prozesse:
+
+Für übersichtlichte Liste
+pm2 ls
+
+für ausführliche Übersicht
+pm2 monit 
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const users = require('./routes/users');
-//const apartments = require('./routes/apartments');
-//const shoppingLists = require('./routes/shoppingLists');
-
-const url = 'mongodb://127.0.0.1:27017';
-//mongoose.connect(url);
-
-var app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-// User Management
-app.use('/api/users', users);
-
-// Apartment Management
-//app.use('/api/apartments', apartments);
-
-// Shopping List Management
-//app.use('api/shoppingLists', shoppingLists);
-
-app.listen(3000);
-console.log('Listening on port 3000...');
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
